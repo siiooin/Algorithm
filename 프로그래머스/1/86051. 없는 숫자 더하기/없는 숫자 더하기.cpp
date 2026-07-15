397236ea2c3f0c6a4e8a7d9da2b6ceb6b54b1a24
siiooin <sssyyy0716@naver.com>
2026-07-15 11:41:08 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+    int sum = 45;
+    
+    for(int i=0;i<numbers.size();i++){
+        sum -= numbers[i];
+    }
+    
+    answer = sum;
+    return answer;
+}
