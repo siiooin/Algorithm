ea28e052889451270ccf496046647cb6576391e3
siiooin <sssyyy0716@naver.com>
2026-07-31 00:09:18 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 8.15 ms, Memory: 13.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    int min = arr[0];
+    
+    if(arr.size() <= 1){
+        return {-1};
+    }
+    
+    for(int i=0; i < arr.size(); i++){
+        if(arr[i] <= min){
+            min = arr[i];
+        }
+    }
+    
+    for(int i=0; i < arr.size(); i++){
+        if(arr[i] != min){
+            answer.push_back(arr[i]);
+        }
+    }
+   
+    return answer;
+}
